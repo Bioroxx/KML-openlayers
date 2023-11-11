61952cbf88a4b094f9a7179f237a8e4714e5c054
David Posch <david.posch@outlook.at>
2023-11-11 15:54:00 +0100

    #1 Install OpenLayers and create empty module with component

---
+++ src/app/module/open-layers/component/open-layers-map/open-layers-map.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-open-layers-map',
+  templateUrl: './open-layers-map.component.html',
+  styleUrls: ['./open-layers-map.component.scss']
+})
+export class OpenLayersMapComponent {
+
+}
---
+++ src/app/module/open-layers/open-layers.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OpenLayersMapComponent } from './component/open-layers-map/open-layers-map.component';
+
+
+
+@NgModule({
+  declarations: [
+    OpenLayersMapComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class OpenLayersModule { }
